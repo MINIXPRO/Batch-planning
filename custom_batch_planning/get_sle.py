638feec674533c448ffe279cb6eef7cb2b295666
Shivam1Singh1 <shivamsingh16mar@gmail.com>
2026-07-14 06:33:55 +0000

    remove corrupted get_sle.py files (null bytes causing SyntaxError)

--- custom_batch_planning/get_sle.py
+++
@@ -1,5 +0,0 @@
-import frappe
-def execute():
-    entries = frappe.db.get_all("Stock Ledger Entry", filters={"voucher_no": "PR-2026-2027-00010"}, fields=["name", "item_code", "batch_planning_id", "project", "actual_qty", "warehouse", "voucher_type"])
-    for entry in entries:
-        print(entry)
